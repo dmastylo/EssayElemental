f5766241299d0f50ce83325e596f4a4e5ed38c37
Damian Mastylo <dmastylo@gmail.com>
2012-11-15 01:30:15 -0500

    Lot's of additions: bug fixes, actual functioning essay builder, and more

--- js/generateSlides.js
+++ js/generateSlides.js
@@ -29,18 +29,18 @@ slides[slides.length] = {
 };
 
 slides[slides.length] = {
-    title: "Opening Paragraph: Thesis",
-    question: "In one sentence, tell the reader what your opinion on the subject of [title] is.",
-    inputField: "textarea",
-    inputName: "thesis"
-};
-
-slides[slides.length] = {
     title: "Opening Paragraph: Supporting Sentences",
     question: "Talk about some details that prove your point, aim for 5.",
     numFields: 5,
     inputField: "textarea",
     inputName: "openParaSupporting"
+};
+
+slides[slides.length] = {
+    title: "Opening Paragraph: Thesis",
+    question: "In one sentence, tell the reader what your opinion on the subject of [title] is.",
+    inputField: "textarea",
+    inputName: "thesis"
 };
 
 slides[slides.length] = {
@@ -60,41 +60,78 @@ slides[slides.length] = {
 $(document).ready(function() {
     for (i = 0; i < slides.length; ++i)
     {
-        var toAppend = [];
-        if (i === 0)
-        {
-            toAppend = ['<div class="item active">', '<div class="container">', '<div class="carousel-caption">'];
-        }
-        else
-        {
-            toAppend = ['<div class="item">', '<div class="container">', '<div class="carousel-caption">'];
-        }
-
-        toAppend[toAppend.length] = '<h2>' + slides[i].title + '</h2>';
-        toAppend[toAppend.length] = '<p class="lead">' + slides[i].question + '</p>';
+        var numFields;
         if (slides[i].hasOwnProperty("numFields"))
         {
-            for (j = 0; j < slides[i].numFields; ++j)
-            {
-                toAppend[toAppend.length] = '<br><textarea name="' + slides[i].inputName + j + '" id="' + slides[i].inputName + j + '" type="' + slides[i].inputField + j + '" class="supporting" wrap="soft"></textarea><br>';
-            }
+            numFields = slides[i].numFields;
         }
         else
         {
+            numFields = 1;
+        }
+
+        for (j = 0; j < numFields; ++j)
+        {
+            var slide = [];
+
+            if (i === 0)
+            {
+                slide = ['<div class="item active">', '<div class="container">', '<div class="carousel-caption">'];
+            }
+            else
+            {
+                slide = ['<div class="item">', '<div class="container">', '<div class="carousel-caption">'];
+            }
+
+            if (numFields > 1)
+            {
+                slide[slide.length] = '<h2>' + slides[i].title + ' ' + (j + 1) + '</h2>';
+            }
+            else
+            {
+                slide[slide.length] = '<h2>' + slides[i].title + '</h2>';
+            }
+
+            slide[slide.length] = '<p class="lead">' + slides[i].question + '</p>';
+            
             if (slides[i].inputField === "text")
             {
-                toAppend[toAppend.length] = '<br><input name="' + slides[i].inputName + '" id="' + slides[i].inputName + '" type="' + slides[i].inputField + '"><br>';
+                slide[slide.length] = '<br><input name="' + (slides[i].inputName + j) + '" id="' + (slides[i].inputName + j) + '" type="' + slides[i].inputField + '"><br>';
             }
             else if (slides[i].inputField === "textarea")
             {
-                toAppend[toAppend.length] = '<br><textarea name="' + slides[i].inputName + '" id="' + slides[i].inputName + '" type="' + slides[i].inputField + '" wrap="soft"></textarea><br>';
+                slide[slide.length] = '<br><textarea name="' + (slides[i].inputName + j) + '" id="' + (slides[i].inputName + j) + '" type="' + slides[i].inputField + '" wrap="soft"></textarea><br>';
+            }
+
+            slide[slide.length] = '<a id="' + (slides[i].inputName + j) + 'Submit" class="btn btn-large btn-primary">Submit</a>';
+            slide[slide.length] = '</div>';
+            slide[slide.length] = '</div>';
+            slide[slide.length] = '</div>';
+            
+            $('.carousel-inner').append(slide.join(''));
+            if (i < 3)
+            {
+                if (slides[i].inputName === "title")
+                {
+                    $('#essay').append('<br><center><span id="' + (slides[i].inputName + j) + 'Output">');
+                }
+                else
+                {
+                    $('#essay').append('<br><span id="' + (slides[i].inputName + j) + 'Output">');
+                }
+            }
+            else
+            {
+                $('#essay').append('<span id="' + (slides[i].inputName + j) + 'Output">');
             }
         }
-        toAppend[toAppend.length] = '<a class="btn btn-large btn-primary" href="#">Submit</a>';
-        toAppend[toAppend.length] = '</div>';
-        toAppend[toAppend.length] = '</div>';
-        toAppend[toAppend.length] = '</div>';
-        
-        $('.carousel-inner').append(toAppend.join(''));
     }
+
+    $(".btn").click(function () {
+        var spanName = $(this).attr("id").replace('Submit', 'Output');
+        var inputName = $(this).attr("id").replace('Submit', '');
+        // Clear first
+        $("#" + spanName).html('');
+        $("#" + spanName).append($("#" + inputName).val());
+    });
 });
